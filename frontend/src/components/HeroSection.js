b25afcd0547a74517fe957f0e9feb665299c0c53
emina1311 <76981459+emina1311@users.noreply.github.com>
2021-01-06 17:13:45 +0100

    Add files via upload

---
+++ frontend/src/components/HeroSection.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import '../App.css';
+
+import './HeroSection.css';
+import Datum from './Datum'
+
+
+function HeroSection() {
+  
+    
+  return (
+    <div className='hero-container'>
+      
+      
+      
+      <div className='clr-entr-point'>
+
+      <div className='reservation-div'>
+        
+          <Datum/>
+        
+        
+      </div>
+      
+      </div>
+    </div>
+    
+  );
+}
+
+export default HeroSection;
