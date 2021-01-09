fb464dfac69515f149861d4a4ebe72cc133b28ba
emina1311 <76981459+emina1311@users.noreply.github.com>
2021-01-09 18:20:56 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Rezervacija from './components/pages/Rezervacija';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import SingleRoom from "./components/pages/SingleRoom";
+import StepOne from './components/pages/StepOne'
+import StepTwo from './components/pages/StepTwo'
+import StepThree from './components/pages/StepThree'
+import StepFour from './components/pages/StepFour'
+
 
 function App() {
   return (
@@ -15,8 +21,15 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/rezervacija' component={Rezervacija} />
+          <Route path='/rezervacija/0' component={StepOne} />
+          <Route path='/rezervacija/1' component={StepTwo} />
+          <Route path='/rezervacija/2' component={StepThree} />
+          <Route path='/rezervacija/3' component={StepFour} />
+          
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          
         </Switch>
       </Router>
     </>
