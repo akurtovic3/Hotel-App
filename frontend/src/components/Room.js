8d4935f68025659404f77708c06c6713622a4203
emina1311 <76981459+emina1311@users.noreply.github.com>
2021-01-10 04:52:05 +0100

    Add files via upload

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -3,17 +3,41 @@ import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
 import PropTypes from "prop-types";
 import { memo } from "react";
+import {useState} from 'react';
+
+import { Button2 } from './Button2';
+import Popup from 'reactjs-popup';
+import '../components/PopUp.css';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { Slide } from 'react-slideshow-image';
+
+import {AiFillStar, AiOutlineStar, AiOutlineWifi} from 'react-icons/ai'
+import {FaRulerCombined, FaSnowflake, FaShower, FaWater,FaGlassCheers} from 'react-icons/fa'
 import {ImUser} from 'react-icons/im'
 import {TiTimes} from 'react-icons/ti'
 import {IoIosBed, IoMdBed} from 'react-icons/io'
-import { Button2 } from './Button2';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-import AOS from 'aos';
-import "aos/dist/aos.css";
+import {RiParkingBoxLine,RiSafeFill} from 'react-icons/ri'
+import {CgScreen, CgTrees} from 'react-icons/cg'
+import {MdKitchen  } from 'react-icons/md'
+import {FcCheckmark} from 'react-icons/fc'
+
 
 const Room = memo(({ room }) => {
-  const { name, slug, images, price, capacity } = room;
+  const { name, slug, images, price, capacity, description, extras, size, pets, breakfast, rates, pogled, tv, minibar, fridge, balkon } = room;
+  const [main, ...defaultImages] = images;
+  console.log(defaultImages);
+  const [dorucak, setDorucak] = useState(false);
+  const [rucak, setRucak] = useState(false);
+  const [vecera, setVecera] = useState(false);
+  const [spa, setSpa] = useState(false);
+  const [bazen, setBazen] = useState(false);
+  const [color1, setColor1] = useState('#D3D3D3'); const [color2, setColor2] = useState('#D3D3D3'); 
+  const [color3, setColor3] = useState('#D3D3D3'); const [color4, setColor4] = useState('#D3D3D3');
+  const [color5, setColor5] = useState('#D3D3D3');
+  
+  
+
   AOS.init({
     // initialise with other settings
     duration : 2000
@@ -28,9 +52,95 @@ const Room = memo(({ room }) => {
             <img src={images[0] || defaultImg} class="image"  alt="single room" width="490px" />
             
             <div className="middle">
-            <Link to={`/rooms/${slug}`} className="btn-primary-room-link">
-              <button type="button" class="btn btn-outline-secondary">Saznajte više</button>
-              </Link>
+            
+            <Popup trigger={<button type="button" class="btn btn-outline-secondary">Saznajte više</button>} position="right center">
+              <div>
+              <h className="room-name">{name}</h>
+              <p>
+              {(rates===1) ? <> <AiFillStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></>
+              : (rates===2) ? <> <AiFillStar/><AiFillStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></>
+              : (rates===3) ? <> <AiFillStar/><AiFillStar/><AiFillStar/><AiOutlineStar/><AiOutlineStar/></> 
+              : (rates===4) ? <>  <AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiOutlineStar/> </>
+              : <>  <AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/> </>  }
+              </p>
+                <section className="single-room">
+
+                  <div className="single-room-images">
+                  <div className="slide-container">
+                  <Slide scale={0.4}>
+                  
+                    {images.map((item, index) => (
+                      <img key={index} src={item} alt={name} width="600px" />
+                    ))}
+                    
+                    </Slide>
+                    </div>
+                  </div>
+                  <div className="single-room-info">
+                    <div className="left">
+                    <article className="info">
+                        <h3>Osnovne informacije</h3>
+                        <h4>Veličina : <FaRulerCombined/> {size} m² </h4>
+                        <h4>
+                          Kapacitet : {capacity} osoba/e
+                        </h4>
+                        <h4>Kućni ljubimci: {pets ? "Dozvoljeno" : "Nije dozvoljeno"}</h4>
+                    </article>
+                        <br/>
+                    <section className="room-extras">
+                      <h3>Ostalo </h3>
+                      <ul className="extras">
+                        {extras.map((item, index) => (
+                          <li key={index}> <FcCheckmark/> {item}</li>
+                        ))}
+                      </ul>
+                    </section>
+                    </div> 
+                    <div className="right">  
+                      <article className="desc">
+                        <h3>Detalji</h3>
+                        <p>
+                          <AiOutlineWifi/> besplatan Wi-Fi <br/>
+                          <RiParkingBoxLine/> besplatan parking <br/>
+                          <FaSnowflake/> klima uređaj <br/>
+                          <FaShower/> vlastito kupatilo <br/>
+                          <RiSafeFill/> sef <br/>
+                          {(pogled===1) ? <> <FaWater /> pogled na more</> : <> <CgTrees/> pogled na vrt  </>  } <br/>
+                          {(tv===1) ? <> <CgScreen/> LCD TV</> : <>   </>} <br/>
+                          {(minibar===1) ? <> <FaGlassCheers/> minibar</> : <>   </>} <br/>
+                          {(fridge===1) ? <> <MdKitchen/> mini firižider</> : <>   </>} <br/>
+                        </p>
+                      </article>
+                      
+                    </div>
+                  </div>
+                </section>
+                <div className="single-room-info">
+                  <div className="left">
+                  <button className="btn-group" style={{background:color1}} onClick={() => {setDorucak(!dorucak);  setColor1(dorucak ? '#009933' : '#D3D3D3'); }}>
+                    Doručak+
+                  </button>
+
+                  <button className="btn-group" style={{background:color2}} onClick={() => {setRucak(!rucak);  setColor2(rucak ? '#009933' : '#D3D3D3'); }}>
+                    Ručak+
+                  </button>
+                  <button className="btn-group" style={{background:color3}} onClick={() => {setVecera(!vecera);  setColor3(vecera ? '#009933' : '#D3D3D3'); }}>
+                    Večera+
+                  </button>
+                  <button className="btn-group" style={{background:color4}} onClick={() => {setSpa(!spa);  setColor4(spa ? '#009933' : '#D3D3D3'); }}>
+                    Spa+
+                  </button>
+                  <button className="btn-group" style={{background:color5}} onClick={() => {setBazen(!bazen);  setColor5(bazen ? '#009933' : '#D3D3D3'); }}>
+                    Bazen+
+                  </button>
+                  </div>
+                  <div className="rigt">
+                    <h className="cijena">Cijena bez <br/> dodatnih ponuda : <br/> {price} €</h>
+                  </div>
+                </div>
+                
+              </div>
+            </Popup>
               
             </div>
             <div className="price-top">
